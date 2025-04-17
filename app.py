358d342d2dd581ab20562ac4363c198c6dbafd82
falckRepos <liseyPanda@users.noreply.github.com>
2025-04-17 12:28:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def db_connection():
 
 # DNS URLs to HQ and Cloud (now HTTPS over 8080)
 HQ_URL = "https://hq-server-git-kompose-ndrc.apps.osc-hq.hq.ndrc.mil:8080/update"
-CLOUD_URL = "https://cloud-server-git-kompose-ndrc.apps.osc-cloud.cloud.ndrc.mil:8080/update"
-ELASTICSEARCH_URL = "http://elasticsearch-server-git-kompose-ndrc.apps.osc-elasticsearch.elasticsearch.ndrc.mil:9200";
+CLOUD_URL = "https://cloud-server-git-kompose-ndrc.apps.osc-trailer.trailer.ndrc.mil:8080/update"
+ELASTICSEARCH_URL = "http://elasticsearch-route-kompose-ndrc.apps.osc-trailer.trailer.ndrc.mil:9200";
 
 scheduler = APScheduler()
 latest_events = []
